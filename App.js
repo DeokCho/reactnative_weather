c459386a2aeec1f8e318129f4732f23aa4421f2f
조덕행 <60154655+limup3@users.noreply.github.com>
2020-10-08 11:26:48 +0900

    2020.10.08

--- App.js
+++ App.js
@@ -5,17 +5,38 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box1}>
+        <Text style={styles.topFont}>Hello!!</Text>
+      </View>
+      <View style={styles.box2}>
+        <Text style={styles.bottomFont}>World!!</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1
+  },
+  topFont: {
+    color: 'black',
+    fontSize:'50px'
+  },
+  bottomFont: {
+    color: 'white',
+    fontSize:'50px'
+  },
+  box1:{
+    flex:1,
+    backgroundColor:'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box2:{
+    flex:3,
+    backgroundColor:'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
---
+++ components/loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Loading = () =>{
+    return (
+    <View>
+
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
